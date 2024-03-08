771b4efd877ff4048e8412957565d8996781340e
William Reeder <whhriv@gmail.com>
2024-03-08 11:16:15 -0800

    Got routing working correctly... Still working on input form route

--- destinations/react/smartroute/src/components/Map.jsx
+++ destinations/react/smartroute/src/components/Map.jsx
@@ -1,24 +1,16 @@
 import GoogleMapReact from 'google-map-react'
 
+const Map = ({ center, zoom }) => {
 
-
-
-const Map = ({ eventData, center, zoom }) => {
-    const markers = eventData.map((ev) => {
-        if (ev.categories[0].id === 8) {
-            return  <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
-        }
-        return null
-    })
   return (
     <div className="map">
         <GoogleMapReact
-            bootstrapURLKeys={{key: 'AIzaSyCxNcH1SVgXN7ySWeNtK-L48wc5N1_7XzQ'}}
+            bootstrapURLKeys={{key: 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg'}}
             defaultCenter={ center }
             defaultZoom = { zoom }
         >
          
-            {markers}
+           
         </GoogleMapReact>
         </div>
   )
